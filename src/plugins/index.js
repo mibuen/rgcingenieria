03c45dcc0ec0e6bd7bd896ee7274aeda91619e3b
Manuel Segovia <msegovia@mac.com>
2021-06-09 21:43:37 -0400

    removed laabr

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -4,9 +4,9 @@ exports.plugin = {
   register: async (server, options) => {
     await server.register([
       { plugin: require('blipp') },
-      {
-        plugin: require('laabr'),
-      },
+      // {
+      //   plugin: require('laabr'),
+      // },
       { plugin: require('./mongoPlugin') },
 
     ]);
