6864881c56c670b7e37c8731475e5c7e7dd6fc54
Manuel Segovia <msegovia@mac.com>
2021-06-21 19:15:05 -0400

    changed client format

--- src/services/uploadV4.js
+++ src/services/uploadV4.js
@@ -3,10 +3,10 @@ const { createPresignedPost } = require('@aws-sdk/s3-presigned-post');
 const { v4 } = require('uuid');
 
 const client = new S3Client({
+  region: 'us-east-1',
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: 'us-east-1',
   },
 });
 const uploadS3v4 = async (proyectoId, key) => {
